27c9dfbfa0c80eff643a33a08ea59d5741be3223
dasifue <dasifue@gmail.com>
2024-07-02 19:57:29 +0600

    cart management

--- cart/urls.py
+++ cart/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 
 from .views import (
     get_cart,
-    add_to_cart
+    add_to_cart,
+    change_quantity,
+    delete_from_cart,
 )
 
 
 urlpatterns = [
     path("", get_cart, name="get_cart"),
-    path("add/<int:clothes_id>", add_to_cart, name="add_to_cart")
+    path("add/<int:clothes_id>", add_to_cart, name="add_to_cart"),
+    path("change/quantity/<int:cart_product_id>", change_quantity, name="change_quantity"),
+    path("delete/<int:cart_product_id>", delete_from_cart, name="delete_from_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -19,9 +19,31 @@ def add_to_cart(request, clothes_id):
     clothes = get_object_or_404(Clothes, id=clothes_id)
     quantity = request.GET.get("quantity", 1)
 
-    CartProduct.objects.create(
-        cart=request.user.cart,
-        clothes=clothes,
-        quantity=quantity,
-    )
+    for product in request.user.cart.products.all():
+        if product.clothes == clothes:
+            product.quantity += int(quantity)
+            product.save()
+            break
+    else:
+        CartProduct.objects.create(
+            cart=request.user.cart,
+            clothes=clothes,
+            quantity=quantity,
+        )
+    return redirect(request.META.get("HTTP_REFERER", "/"))
+
+
+@login_required
+def change_quantity(request, cart_product_id):
+    cart_product = get_object_or_404(CartProduct, id=cart_product_id)
+    quantity = request.GET["quantity"]
+
+    cart_product.quantity = abs(int(quantity))
+    cart_product.save()
+    return redirect(request.META.get("HTTP_REFERER", "/"))
+
+@login_required
+def delete_from_cart(request, cart_product_id):
+    cart_product = get_object_or_404(CartProduct, id=cart_product_id)
+    cart_product.delete()
     return redirect(request.META.get("HTTP_REFERER", "/"))
